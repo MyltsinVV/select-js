355dc83a5da60a4d242c275f19290163115aea60
vovam <vova.myltsin@yandex.ru>
2020-04-20 09:04:52 +0400

    createElements: arrow and popup

--- index.js
+++ index.js
@@ -9,50 +9,66 @@ class Select {
   constructor(param) {
     this.rootElement = document.querySelector(param.selector)
     this.label = param.label
-    this.init()
+    this.countOpenSelect = 0;
+
+    this.createElements()
     this.events()
   }
 
-  /**
-   * init select
-   */
-  init() {
+  createElements() {
     // create label
     let label = document.createElement('div')
     label.innerText = this.label
     label.classList.add('label')
-
     this.rootElement.append(label)
-    this.labelElement = label
-
-
 
     // create select
     let select = document.createElement('div')
     select.classList.add('select')
     this.rootElement.append(select)
-    this.selectElement = select
 
-    this.countOpenSelect = 0;
-    select.onclick = () => {
+    //create arrow
+    let arrow = document.createElement('div')
+    arrow.classList.add('arrow')
+    select.append(arrow)
+
+    // create popup
+    let popup = document.createElement('div')
+    popup.classList.add('popup')
+    this.rootElement.append(popup)
+
+    // create loader
+    let loader = document.createElement('div')
+    loader.classList.add('option', 'loader')
+    loader.innerText = '123'
+    popup.append(loader)
+
+    // save elements
+    this.selectElement = select
+    this.labelElement = label
+    this.popupElement = popup
+    this.arrowElement = arrow
+  }
+
+  events() {
+    // click to label
+    this.labelElement.onclick = () => {
+      if (this.countOpenSelect === 0) {
+        this.selectElement.click()
+      }
+    }
+    // click to select
+    this.selectElement.onclick = () => {
       if (this.countOpenSelect === 1) {
         this.closeSelect()
         return
       }
-
       this.openSelect()
     }
-    select.onblur = () => {
+    // blur on select
+    this.selectElement.onblur = () => {
       if (this.countOpenSelect === 1) {
         this.closeSelect()
-      }
-    }
-  }
-
-  events() {
-    this.labelElement.onclick = () => {
-      if (this.countOpenSelect === 0) {
-        this.selectElement.click()
       }
     }
   }
@@ -60,11 +76,15 @@ class Select {
   openSelect() {
     this.countOpenSelect++
     this.labelElement.classList.add('topLabel')
+    this.popupElement.classList.add('show')
+    this.arrowElement.classList.add('top')
   }
 
   closeSelect() {
     this.countOpenSelect = 0
     this.labelElement.classList.remove('topLabel')
+    this.popupElement.classList.remove('show')
+    this.arrowElement.classList.remove('top')
   }
 }
 
